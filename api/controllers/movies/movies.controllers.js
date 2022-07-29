826afcf441c35d9ea3619e37638d2cf0ac9ee2fd
adi-peled <adipeled499@gmail.com>
2022-07-29 17:59:46 +0300

    cache

--- api/controllers/movies/movies.controllers.js
+++ api/controllers/movies/movies.controllers.js
@@ -1,31 +1,24 @@
 import { ResService } from "../../../services/resHandler.service.js";
 import axios from 'axios'
 import dotenv from 'dotenv'
-import fs from 'fs'
-import movies from '../../../data/movies.json' assert {type: "json"};
+import NodeCache from 'node-cache'
 dotenv.config()
 const base_url = 'https://www.omdbapi.com?type=movie'
 const apikey = process.env.apikey
-console.log({apikey});
+const myCache = new NodeCache({ stdTTL: 60 * 60 * 24 })
 export class MovieController {
 
     static async getDefault(req, res) {
-        console.log('getDefault');
         try {
-            if (movies.length) {
-                ResService.handleSuccess(res, movies)
+            if (myCache.has('movies')) {
+                ResService.handleSuccess(res, JSON.parse(myCache.get('movies')))
             } else {
-                const movies = await axios.get(`${base_url}&&s=ince&apikey=${apikey}`)
+                const movies = await axios.get(`${base_url}&&s=the k&apikey=${apikey}`)
                 if (movies?.data?.Search) {
-                    fs.writeFile('data/movies.json', JSON.stringify(movies.data.Search), 'utf-8', (err) => {
-                        console.log({ err });
-                        if (err) {
-                            console.log('err', err);
-                        } else {
-                            console.log('saved');
-                        }
-                        ResService.handleSuccess(res, movies.data.Search)
-                    })
+                    myCache.set('movies', JSON.stringify(movies.data.Search))
+                    ResService.handleSuccess(res, movies.data.Search)
+                } else {
+                    ResService.handleSuccess(res, movies.data)
                 }
             }
         } catch (err) {
@@ -36,7 +29,6 @@ export class MovieController {
     static async searchByName(req, res) {
         try {
             const { title } = req.query
-            console.log(title);
             const movies = await axios.get(`${base_url}&&s=${title}&apikey=${apikey}`)
             ResService.handleSuccess(res, movies.data)
         } catch (err) {
@@ -48,8 +40,16 @@ export class MovieController {
     static async getFullInfo(req, res) {
         try {
             const { movieId } = req.params
-            const movie = await axios.get(`${base_url}&&i=${movieId}&apikey=${apikey}`)
-            ResService.handleSuccess(res, movie.data)
+            let fullMovie = null
+            if (myCache.has(movieId)) {
+                fullMovie = JSON.parse(myCache.get(movieId))
+            } else {
+                const movie = await axios.get(`${base_url}&&i=${movieId}&apikey=${apikey}`)
+                fullMovie = movie.data
+                myCache.set(movieId, JSON.stringify(fullMovie))
+            }
+            ResService.handleSuccess(res, fullMovie)
+
         } catch (err) {
             ResService.handleErr(res, err)
         }
